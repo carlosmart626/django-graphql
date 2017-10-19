73209dbc533cf3fb9ffe57985c19cd93b3fe2a87
Carlos Martinez <carlosmart626@gmail.com>
2017-10-19 13:50:30 -0500

    Update tests example

--- starwars/characters/tests.py
+++ starwars/characters/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from api.schemas import schema
+
+
+class GraphQLTests(TestCase):
+    """
+    This TestCase auth.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        super(GraphQLTests, cls).setUpClass()
+        cls.schema = schema
+        cls.query_node = '''
+            query{
+              hello(name: "Carlos")
+            }
+        '''
+
+    def test_query(self):
+        result = self.schema.execute(self.query_node)
+        self.assertNotEqual(result.errors, [])
