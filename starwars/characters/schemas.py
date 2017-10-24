6b80ea4cfe9eceafc7947bb579523ec6f24b56ff
Carlos Martinez <carlosmart626@gmail.com>
2017-10-24 16:31:06 -0500

    Update resolver

--- starwars/characters/schemas.py
+++ starwars/characters/schemas.py
@@ -2,9 +2,16 @@ from graphene import relay, String, List, resolve_only_args
 from graphene_django import DjangoObjectType
 
 from .models import HumanCharacter, DroidCharacter
+from movies.schemas import MovieNode
 
 
 class HumanCharacterNode(DjangoObjectType):
+
+    appears_in = relay.ConnectionField(MovieNode, description='Apariciones')
+
+    @resolve_only_args
+    def resolve_appears_in(self):
+        return self.appears_in.all()
 
     class Meta:
         model = HumanCharacter
